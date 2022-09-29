e2a01d457529ef24d6fd8edd2f7428cefb5aa5f3
ANDRÉ SILVA BARRETO JUNIOR <andre_barreto-junior@estudante.sesisenai.org.br>
2022-09-29 20:33:05 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from
+    '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Login from './src/screens/Login.js';
+import RegisterUser from './src/screens/RegisterUser.js';
+const Stack = createNativeStackNavigator();
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }} >
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="RegisterUser" component={RegisterUser} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    App: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
 });
+export default () => {
+    return (
+        <SafeAreaProvider>
+            <App />
+        </SafeAreaProvider>
+    );
+};
